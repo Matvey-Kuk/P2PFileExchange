9fc7405fa80aa457714d9c9575d6552ee90e3596
CotletkaMan <iaklalnavas@mail.ru>
2014-03-05 18:04:23 +0400

    Доработанный обход папки

--- FileProcessingModule/OpenDir.py
+++ FileProcessingModule/OpenDir.py
@@ -1,6 +1,21 @@
+import os
 
 class OpenDir():
-    def __init__(self, path):
-        self.path = path
-    def see_dir(self):
-        pass
+    def __init__(self):
+        self.files_in_dir = {}
+    def see_dir(self, path):
+        try:
+            self.files_in_dir[path] = os.listdir(path)
+        except:
+            return False
+        else:
+            for i, file in enumerate(self.files_in_dir[path]):
+                if(self.see_dir(path + '/' + file) == True):
+                    del self.files_in_dir[path][i]
+                    if len(self.files_in_dir[path]) == 0 :
+                        del self.files_in_dir[path]
+            return True
+
+test = OpenDir()
+test.see_dir('base')
+print(test.files_in_dir)
