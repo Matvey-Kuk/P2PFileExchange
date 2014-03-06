cd84998824fcea7662ebb987720fabaa612906e9
CotletkaMan <iaklalnavas@mail.ru>
2014-03-06 13:28:15 +0400

    Немножко динамики

--- FileProcessingModule/OpenDir.py
+++ FileProcessingModule/OpenDir.py
@@ -1,21 +1,19 @@
 import os
 
+
 class OpenDir():
-    def __init__(self):
+    def __init__(self, path):
         self.files_in_dir = {}
+        self.see_dir(path)
+
     def see_dir(self, path):
         try:
-            self.files_in_dir[path] = os.listdir(path)
-        except:
+            self.files_in_dir[path] = [x for x in os.listdir(path) if x[-1] != '~']
+        except NotADirectoryError:
             return False
         else:
             for i, file in enumerate(self.files_in_dir[path]):
-                if(self.see_dir(path + '/' + file) == True):
-                    del self.files_in_dir[path][i]
-                    if len(self.files_in_dir[path]) == 0 :
-                        del self.files_in_dir[path]
+                if os.path.isdir(path + '/' + file):
+                     self.see_dir(path + '/' + file)
+            self.files_in_dir[path] = [x for x in self.files_in_dir[path] if os.path.isfile(path + '/' + x)]
             return True
-
-test = OpenDir()
-test.see_dir('base')
-print(test.files_in_dir)
--- FileProcessingModule/threadControl.py
+++ FileProcessingModule/threadControl.py
@@ -1,17 +1,14 @@
-__author__ = 'lossidze'
 
-from openfile  import diffile
+from openfile  import Diffile
 from threading import Thread
 
 class threadControl(Thread):
-	"""Выделяем поток по заданную задачу"""
     def __init__(self):
         Thread.__init__(self)
         self.daemon = True
-        self.dif = diffile()
+        self.dif = Diffile()
     def run(self):
         self.dif.find_diff()
 
-if ("__name__" == "__main__"):
-	thread = threadControl()
-	thread.start()
+thread = threadControl()
+thread.start()
