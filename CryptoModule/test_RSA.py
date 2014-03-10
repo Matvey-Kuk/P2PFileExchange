8a7be5252aac5e4ceddd75f91dd53bbff74dc8b8
Radmir <kar.radmir@gmail.com>
2014-03-10 10:40:53 +0400

    URA

--- CryptoModule/test_RSA.py
+++ CryptoModule/test_RSA.py
@@ -2,11 +2,9 @@
 
 import rsa
 (pubkey, privkey) = rsa.newkeys(512)
-# print(pubkey)
-# print(privkey)
-send_message = 'Ti kto takoy'
+send_message = b'Ti kto takoy'
 crypto = rsa.encrypt(send_message, pubkey)
 reseive_message = rsa.decrypt(crypto, privkey)
-# print(send_message,'\n',reseive_message)
+
 
 
