d19e53708364100e2ee73a8dc678c4a822ed0b8c
Radmir <kar.radmir@gmail.com>
2014-03-10 17:03:26 +0400

    aga

--- CryptoModule/test_RSA.py
+++ CryptoModule/test_RSA.py
@@ -1,2 +1,8 @@
 __author__ = 'Radmir'
 
+import rsa
+(pubkey, privkey) = rsa.newkeys(512)
+send_message = b'Ti kto takoy'
+crypto = rsa.encrypt(send_message, pubkey)
+reseive_message = rsa.decrypt(crypto, privkey)
+print(reseive_message)
