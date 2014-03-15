d9f4e8a8db61a1193c29abc73ae509770e83b2c0
Matvey-Kuk <motakuk@gmail.com>
2014-03-15 13:54:39 +0400

    Добавил ключи включения только одного модуля

--- Main.py
+++ Main.py
@@ -13,9 +13,22 @@ class Main(object):
         self.command_line_arguments = self.parse_arguments()
         self.networking = self.start_networking()
         self.requests_processor = RequestsProcessor(self.networking)
-        self.auth_database = AuthDataBase(self.networking, self.requests_processor)
-        self.connection_circle_detector = ConnectionCircleDetector(self.networking, self.requests_processor)
-        self.p2p = P2p(self.networking, self.requests_processor)
+
+        only_one_module_enabled = \
+            self.command_line_arguments.onlyAuthDatabaseModule or \
+            self.command_line_arguments.onlyP2PModule
+
+        if self.command_line_arguments.onlyAuthDatabaseModule:
+            self.auth_database = AuthDataBase(self.networking, self.requests_processor)
+
+        if self.command_line_arguments.onlyP2PModule:
+            self.connection_circle_detector = ConnectionCircleDetector(self.networking, self.requests_processor)
+            self.p2p = P2p(self.networking, self.requests_processor)
+
+        if not only_one_module_enabled:
+            self.auth_database = AuthDataBase(self.networking, self.requests_processor)
+            self.connection_circle_detector = ConnectionCircleDetector(self.networking, self.requests_processor)
+            self.p2p = P2p(self.networking, self.requests_processor)
 
     @staticmethod
     def parse_arguments():
@@ -23,7 +36,18 @@ class Main(object):
         parser.add_argument('-port', '-p', dest='port', help='Server port')
         parser.add_argument('-host', dest='bind_host', help='Host for server binding')
         parser.add_argument('-peer', dest='first_peer', help='ip:port of first peer needed for connection')
-        parser.add_argument('-nop2pModule', dest='nop2pModule', help='Disable p2p module.')
+        parser.add_argument(
+            '-onlyAuthDatabaseModule',
+            dest='onlyAuthDatabaseModule',
+            action='store_true',
+            help='Enables only one module.'
+        )
+        parser.add_argument(
+            '-onlyP2PModule',
+            dest='onlyP2PModule',
+            action='store_true',
+            help='Enables only one module.'
+        )
 
         command_line_args = parser.parse_args()
         return command_line_args
