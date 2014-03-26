b630d313b127f2d74d931ac89154b12cf5b21016
anLENIN <anLENIN@gmail.com>
2014-03-26 06:25:52 +0400

    #14 Метод send_data_to_interface() должен возвращать информационную строку Она выводится в верхнее поле Пока дублируется и в поле "командной строки" Думаю над оформлением, как лучше размещать информацию в верхнем поле

--- Main.py
+++ Main.py
@@ -33,6 +33,8 @@ class Main(object):
 
         if not self.command_line_arguments.noInterface:
             self.interface = Interface()
+            self.interface.register_data_sender(self.p2p, self.p2p)
+            self.interface.roottk.mainloop()
 
     @staticmethod
     def parse_arguments():
