6803b60df1bf0de18d7307b75670491e135c67b8
Matvey-Kuk <motakuk@gmail.com>
2014-04-15 20:08:09 +0400

    Расширил тест хешей

--- Tests/TestDatabaseEngine.py
+++ Tests/TestDatabaseEngine.py
@@ -36,3 +36,7 @@ class TestDatabaseEngine(unittest.TestCase):
         table_a.new_alteration({'some_key': 'some_value'})
         table_b.new_alteration({'some_key': 'another_value'})
         self.assertNotEqual(table_a.get_hash(), table_b.get_hash())
+
+        table_a.new_alteration({'some_key': 'some_value'})
+        table_b.new_alteration({'some_key': 'some_value'})
+        self.assertEqual(table_a.get_hash(), table_b.get_hash())
