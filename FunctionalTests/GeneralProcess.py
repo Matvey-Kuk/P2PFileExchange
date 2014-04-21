9c721659f0d7a08114e7c06bb10afe78768f9dc5
DmitryEropkin <DmitryEropkin@MacBook-Pro-Dmitry.local>
2014-04-21 10:25:40 +0400

    Тестировщик
    Заработали аргументы

--- FunctionalTests/GeneralProcess.py
+++ FunctionalTests/GeneralProcess.py
@@ -7,13 +7,18 @@ arr = {}
 
 processes = []
 
+k = int(input("Какое количество программ запустить:"))
 
-def creation_process(n):
+
+def creation_process():
     global arr
-    for i in range(n):
+    for i in range(k):
         f = tempfile.TemporaryFile()
         #f = open('text.txt', 'a')
-        subprocess.Popen(['python3 $HOME/network/Main.py -h'], stdout=f, shell=True)
+        if i == 0:
+            subprocess.Popen(['python3 $HOME/network/Main.py -port 111' +str(i+1)+ ' -P2PModule'], stdout=f, shell=True)
+        else:
+            subprocess.Popen(['python3 $HOME/network/Main.py -port 111' +str(i+1)+ ' -peer 127.0.0.1:111' +str(i+1)+ ' -P2PModule'], stdout=f, shell=True)
         processes.append(f)
         arr[i] = f
 
@@ -22,12 +27,12 @@ def creation_process(n):
 def log():
     global arr
     while True:
-        n=int(input("Введите номер экземпляра запущенной программы:"))
+        n = int(input("Введите номер экземпляра запущенной программы:"))
         arr[n-1].seek(0)
         print(arr[n-1].read())
 
 
-t1 = threading.Thread(name='th1', target=creation_process, kwargs={'n': 10})
+t1 = threading.Thread(name='th1', target=creation_process)
 t2 = threading.Thread(name='th2', target=log)
 
 t1.start()
