466ee2ccb31a7bf1ebea94fedda24fbcab364145
Matvey-Kuk <motakuk@gmail.com>
2014-04-27 18:00:19 +0400

    проверка на подпись шарика

--- Tests/UsersDatabase/TestCryptography.py
+++ Tests/UsersDatabase/TestCryptography.py
@@ -3,15 +3,23 @@ import unittest
 import rsa
 
 from UsersDatabaseModule.Cryptography import *
+from DatabaseEngineModule.Alteration import *
 
 
 class TestCryptography(unittest.TestCase):
 
     def setUp(self):
-        pass
+        self.keys = Cryptography.generate_keys()
 
     def test_private_pub_keys(self):
-        keys = Cryptography.generate_keys()
         message = 'Go left at the blue tree'
-        signature = Cryptography.get_signature(message, keys['private_key'])
-        self.assertTrue(Cryptography.verify_signature(message, keys['public_key'], signature))
+        signature = Cryptography.get_signature(message, self.keys['private_key'])
+        self.assertTrue(Cryptography.verify_signature(message, self.keys['public_key'], signature))
+
+    def test_alteration_signing(self):
+        test_alteration = Alteration({'Hello': 'World'}, VersionsRange(version=1))
+        self.assertFalse(Cryptography.is_signed(test_alteration))
+
+        test_alteration = Cryptography.sign_alteration(test_alteration, self.keys['private_key'])
+        self.assertTrue(Cryptography.is_signed(test_alteration))
+
--- UsersDatabaseModule/Cryptography.py
+++ UsersDatabaseModule/Cryptography.py
@@ -1,11 +1,13 @@
 import rsa
 
+from DatabaseEngineModule.Alteration import *
+
 
 class Cryptography(object):
 
     @staticmethod
     def generate_keys():
-        (public_key, private_key) = rsa.newkeys(1024)
+        (public_key, private_key) = rsa.newkeys(512)
         return {
             'public_key': Cryptography.dump_public_key(public_key),
             'private_key': Cryptography.dump_private_key(private_key)
@@ -53,3 +55,31 @@ class Cryptography(object):
             private_key['p'],
             private_key['q']
         )
+
+    @staticmethod
+    def sign_alteration(alteration, private_key):
+        new_changes = {}
+        old_changes = alteration.get_changes()
+        if not alteration.get_versions_range().get_size() == 1:
+            raise Exception('Can sign only alteration with versions range size = 1.')
+        for key in old_changes:
+            new_changes[key] = {
+                'data': old_changes[key],
+                'signature': Cryptography.get_signature(old_changes[key], private_key)
+            }
+        return Alteration(
+            changes=new_changes,
+            versions_range=alteration.get_versions_range(),
+            creation_time=alteration.get_creation_time()
+        )
+
+
+
+    @staticmethod
+    def is_signed(alteration):
+        signed = True
+        changes = alteration.get_changes()
+        for key in changes:
+            if not ('signature' in changes[key] and 'data' in changes[key]):
+                signed = False
+        return signed
