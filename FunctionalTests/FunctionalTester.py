ba44aeefedf74514aa4836142983f7c12fc125f2
Matvey-Kuk <motakuk@gmail.com>
2014-05-17 17:55:05 +0400

    Функциональный тестер запускает процесс и отдает ему команды

--- FunctionalTests/FunctionalTester.py
+++ FunctionalTests/FunctionalTester.py
@@ -10,11 +10,14 @@ parser.add_argument('-path', '-p', dest='path', help='Test program path')
 
 networking = Networking('127.0.0.1', 1234)
 
-
 instance = Instance(
     parser.parse_args().path + '/Main.py',
-    ' -port 1235 -P2PModule -UsersDatabaseModule -functionalTestInteractionPort 1111'
+    1234,
+    1111
 )
+
+sleep(5)
+
 networking.provoke_connection('127.0.0.1', 1111)
 
 for peer in networking.get_peers():
--- FunctionalTests/Instance.py
+++ FunctionalTests/Instance.py
@@ -7,10 +7,20 @@ class Instance(object):
     Этот класс описывает объект, который представляет конкретный запущенный экземпляр.
     """
 
-    def __init__(self, run_command, arguments):
+    def __init__(self, run_command, server_port, functional_testing_port):
+        print('started')
         self.process = subprocess.Popen(
-            ['python3', run_command, arguments],
-            shell=False
+            [
+                'python3',
+                run_command,
+                '-port',
+                str(server_port),
+                '-P2PModule',
+                '-UsersDatabaseModule',
+                '-functionalTestInteractionPort',
+                str(functional_testing_port)
+            ],
+            shell=False,
         )
 
     def send_command(self, command):
