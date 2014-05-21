9178cc702828a073de5f0fabfdee97ab18e0b761
Matvey-Kuk <motakuk@gmail.com>
2014-05-21 21:00:34 +0400

    п2п тесты в пень

--- P2PFileExchange/Tests/Functional/FunctionalTestEngine/FunctionalTestEngine.py
+++ P2PFileExchange/Tests/Functional/FunctionalTestEngine/FunctionalTestEngine.py
@@ -12,15 +12,29 @@ class FunctionalTestEngine(NetworkingUsingModule):
 
     def __init__(self):
         AllowingProcessing().allow_processing = True
+
+        networking = Networking('127.0.0.1', 1110)
+        super().__init__(networking, RequestsProcessor(networking), 'functional_testing')
+        self.register_answer_received_callback('command', self.answer_received)
+
         self.parser = argparse.ArgumentParser(description='Hello, p2p world testing.')
         self.parser.add_argument('-path', '-p', dest='path', help='Test program path')
         self.__server_first_port = 1111
         self.__server_port_increment = self.__server_first_port
         self.__functional_port_increment = 2111
         self.__instances = []
-        networking = Networking('127.0.0.1', 1110)
-        super().__init__(networking, RequestsProcessor(networking), 'functional_testing')
-        self.register_answer_received_callback('command', self.answer_received)
+
+    @staticmethod
+    def is_port_opened(port):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        return sock.connect_ex(('127.0.0.1', port)) == 0
+
+    @staticmethod
+    def find_free_port(start_port):
+        result_port = start_port
+        while not FunctionalTestEngine.is_port_opened(result_port):
+            result_port += 1
+        return result_port
 
     def answer_received(self, request):
         pass
